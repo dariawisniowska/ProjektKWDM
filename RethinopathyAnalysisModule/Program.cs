11db0648daffcf692faafa8753efd0f66ae1c842
Daria Wiśniowska <daria.wisniowska123@gmail.com>
2019-04-06 13:05:30 +0200

    Rethinopathy Classification

--- RethinopathyAnalysisModule/Program.cs
+++ RethinopathyAnalysisModule/Program.cs
@@ -9,26 +9,46 @@ namespace RethinopathyAnalysisModule
 {
     class Program
     {
-        private const string LEU_TEST_FILE = @"leu.ds";
+        private const string DvC_TEST_FILE = @"DvC.ds";
+        private const string DvH_TEST_FILE = @"DvH.ds";
+        private const string HvC_TEST_FILE = @"HvC.ds";
+
+        private const string DvC_MODEL_FILE = @"DvCModel";
+        private const string DvH_MODEL_FILE = @"DvHModel";
+        private const string HvC_MODEL_FILE = @"HvCModel";
 
         static double C = 0.8;
         static double gamma = 0.000030518125;
 
-        static svm_problem _prob;
+        static svm_problem DvC_prob, DvH_prob, HvC_prob;
 
         static void Main(string[] args)
         {
             var path = Environment.CurrentDirectory;
-            string fullPath = System.IO.Path.Combine(path, LEU_TEST_FILE);
+            string DvCPath = System.IO.Path.Combine(path, DvC_TEST_FILE);
+            string DvHPath = System.IO.Path.Combine(path, DvH_TEST_FILE);
+            string HvCPath = System.IO.Path.Combine(path, DvC_TEST_FILE);
 
-            // get data from file
-            // Note that you should always scale your data
-            _prob = ProblemHelper.ReadAndScaleProblem(fullPath);
+            DvC_prob = ProblemHelper.ReadAndScaleProblem(DvCPath);
+            DvH_prob = ProblemHelper.ReadAndScaleProblem(DvHPath);
+            HvC_prob = ProblemHelper.ReadAndScaleProblem(HvCPath);
 
-            var svm = new C_SVC(_prob, KernelHelper.RadialBasisFunctionKernel(gamma), C);
-            var cva = svm.GetCrossValidationAccuracy(5);
+            var DvCsvm = new C_SVC(DvC_prob, KernelHelper.RadialBasisFunctionKernel(gamma), C);
+            var DvHsvm = new C_SVC(DvH_prob, KernelHelper.RadialBasisFunctionKernel(gamma), C);
+            var HvCsvm = new C_SVC(HvC_prob, KernelHelper.RadialBasisFunctionKernel(gamma), C);
 
-            Console.WriteLine(String.Format("Result: {0}", cva.ToString()));
+            var DvCcva = DvCsvm.GetCrossValidationAccuracy(5);
+            var DvHcva = DvHsvm.GetCrossValidationAccuracy(5);
+            var HvCcva = HvCsvm.GetCrossValidationAccuracy(5);
+
+            DvCsvm.Export(System.IO.Path.Combine(path, DvC_MODEL_FILE));
+            DvHsvm.Export(System.IO.Path.Combine(path, DvH_MODEL_FILE));
+            HvCsvm.Export(System.IO.Path.Combine(path, HvC_MODEL_FILE));
+
+            Console.WriteLine(String.Format("DvC Result: {0}", DvCcva.ToString()));
+            Console.WriteLine(String.Format("DvH Result: {0}", DvHcva.ToString()));
+            Console.WriteLine(String.Format("HvC Result: {0}", HvCcva.ToString()));
+
             Console.ReadKey();
         }
     }
